7bbda47c67a40e3088654e60af1a8be5cecbfae7
Ebi (Satoshi Isemura) <spinylobster@outlook.com>
2018-01-11 22:11:24 +0900

    be git directory if not

--- setup-mac.sh
+++ setup-mac.sh
@@ -4,11 +4,21 @@ function main () {
   if [ `is_installed brew` = 'false' ]; then
     homebrew-install
   fi
+
+  if [ ! -e ".git" ]; then
+    git_pull
+  fi
 }
 
 function homebrew-install () {
   sudo xcodebuild -license
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+function git_pull () {
+  git init
+  git remote add origin https://github.com/spinylobster/my-homedir.git
+  GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git pull -set-upstream-to origin master
+  git remote set-url origin git@github.com:spinylobster/my-homedir.git
 }
 
 function is_installed () {
